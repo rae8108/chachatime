0838af6617111e003ea363070176bf38dec97e48
rae8108 <rayanbana01@gmail.com>
2024-10-22 14:28:40 +0100

    created menu component

---
+++ chachatime/src/Menupage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Menupage() {
+    return (
+        <div>
+            <h1>Our drinks</h1>
+            <h2>seasonal drinks</h2>
+        </div>
+    )
+}
